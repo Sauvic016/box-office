682379154acbb76350dd4711e2be058ba7e40a27
Sauvic <sauvicpaulchy@gmail.com>
2021-09-14 17:27:58 +0530

    Added react routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
-// import logo from "./logo.svg";
-// import './App.css';
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-	return <div className="App">Hi</div>;
+	return (
+		<Switch>
+			<Route exact path="/">
+				This is home page
+			</Route>
+			<Route exact path="/starred">
+				This is Starred page
+			</Route>
+			<Route>This is 404 page</Route>
+		</Switch>
+	);
 }
 
 export default App;
